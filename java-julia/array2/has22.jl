f28c77909694e32ab823cbd1981488a2f5d857f5
Mike Gainey <mikeg9b@gmail.com>
2019-12-23 18:00:13 -0600

    array2 problem

--- java-julia/array2/has22.jl
+++ java-julia/array2/has22.jl
@@ -8,3 +8,45 @@ has22([1, 2, 2]) == true
 has22([1, 2, 1, 2]) == false
 has22([2, 1, 2]) == false
 """
+function has22a(ns)
+    lastnumber = ns[1]
+    for n in ns[2:end]
+        if n == 2 && lastnumber == 2
+            return true
+        else
+            lastnumber = n
+        end
+    end
+    return false
+end
+
+function has22b(ns, lastnumber=nothing)
+    if length(ns) == 0
+        return false
+    else
+        head = ns[1]
+        tail = ns[2:end]
+        if head == 2 && lastnumber == 2
+            return true
+        else
+            has22b(tail, head)
+        end
+    end
+end
+
+using Test
+function test()
+    @testset begin
+        @test has22a([1, 2, 2]) == true
+        @test has22a([1, 2, 1, 2]) == false
+        @test has22a([2, 1, 2]) == false
+        @test has22b([1, 2, 2]) == true
+        @test has22b([1, 2, 1, 2]) == false
+        @test has22b([2, 1, 2]) == false
+    end
+    nothing
+end
+
+test()
+
+
