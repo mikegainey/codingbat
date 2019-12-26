811bd444eceb2dabb2904e303261617584b8b9cd
Mike Gainey <mikeg9b@gmail.com>
2019-12-26 17:33:12 -0600

    first array3 problem

---
+++ java-julia/array3/maxspan.jl
@@ -0,0 +1,43 @@
+"""
+https://codingbat.com/prob/p189576
+Array-3 > maxSpan
+
+Consider the leftmost and righmost appearances of some value in an array.
+We'll say that the "span" is the number of elements between the two inclusive.
+A single value has a span of 1. Returns the largest span found in the given array. (Efficiency is not a priority.)
+
+maxspan([1, 2, 1, 1, 3]) == 4
+maxspan([1, 4, 2, 1, 4, 1, 4]) == 6
+maxspan([1, 4, 2, 1, 4, 4, 4]) == 6
+"""
+# with a nested loop
+function maxspan1(ns)
+    steps = 0
+    len = length(ns)
+    maxspan = 0
+    for left in 1 : len-1
+        for right in left+1 : len
+            steps += 1
+            if ns[left] == ns[right]
+                span = right - left + 1
+                if span > maxspan
+                    maxspan = span
+                end
+            end
+        end
+    end
+    @show ns, steps
+    maxspan
+end
+
+using Test
+function test()
+    @testset begin
+        @test maxspan1([1, 2, 1, 1, 3]) == 4
+        @test maxspan1([1, 4, 2, 1, 4, 1, 4]) == 6
+        @test maxspan1([1, 4, 2, 1, 4, 4, 4]) == 6
+    end
+    nothing
+end
+
+test()
