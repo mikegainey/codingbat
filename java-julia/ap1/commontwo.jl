ee7b698e0bb7f8e439c16cde7812cd1e4ece0546
Mike Gainey <mikeg9b@gmail.com>
2020-01-03 04:02:50 -0600

    finished ap1 problems

--- java-julia/ap1/commontwo.jl
+++ java-julia/ap1/commontwo.jl
@@ -11,4 +11,57 @@ commontwo(["a", "c", "x"], ["b", "c", "d", "x"]) == 2
 commontwo(["a", "c", "x"], ["a", "b", "c", "x", "z"]) == 3
 commontwo(["a", "b", "c"], ["a", "b", "c"]) == 3
 """
-function commontwo(as, bs)
+head(xs) = xs[1]
+tail(xs) = xs[2:end]
+
+function commontwo1(as, bs, count=0)
+    if length(as) == 0 || length(bs) == 0
+        return count
+    else
+        if head(as) == head(bs)
+            commontwo1(tail(as), tail(bs), count+1)
+        elseif head(as) < head(bs)
+            commontwo1(tail(as), bs, count)
+        elseif head(bs) < head(as)
+            commontwo1(as, tail(bs), count)
+        else
+            error("Not all cases are covered in the if-elseif block.")
+        end
+    end
+end
+
+function commontwo2(a, b)
+    count = 0
+    ax = 1; bx = 1
+    while ax ≤ length(a) && bx ≤ length(b) # while both indices are valid
+        if a[ax] == b[bx]
+            count += 1
+            ax += 1
+            bx += 1
+        elseif a[ax] < b[bx]
+            ax += 1
+        elseif b[bx] < a[ax]
+            bx += 1
+        else
+            error("Not all cases are covered in the if-elseif block.")
+        end
+    end
+    count
+end
+
+using Test
+function test()
+    @testset begin
+        @test commontwo1(["a", "c", "x"], ["b", "c", "d", "x"]) == 2
+        @test commontwo1(["a", "c", "x"], ["a", "b", "c", "x", "z"]) == 3
+        @test commontwo1(["a", "b", "c"], ["a", "b", "c"]) == 3
+        @test commontwo2(["a", "c", "x"], ["b", "c", "d", "x"]) == 2
+        @test commontwo2(["a", "c", "x"], ["a", "b", "c", "x", "z"]) == 3
+        @test commontwo2(["a", "b", "c"], ["a", "b", "c"]) == 3
+    end
+    nothing
+end
+
+test()
+
+
